bb4758acdb2cb3d4589de4338bc016eecdc6b7c7
marco <marcospiando@gmail.com>
2019-03-04 16:49:13 +0100

    aggiunto ramo e modificato file header

--- PRO2/Main.c
+++ PRO2/Main.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "TSP.h"
+
 int main(int argc, char **argv) {
 	printf("Ci sono %d elementi.\n", argc);
 	for (int i = 0; i < argc; i++) {
--- PRO2/TSP.h
+++ PRO2/TSP.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <stdio.h>
+#include <math.h>
+
+#define VERBOSE 50
+
+typedef struct {
+	int nnodes;
+	double *xcoord;
+	double *ycoord;
+	double timelimit;
+} instance;
