19ba1cb8dc5f9c4fcc31abef1c878370596d91ff
marco <marcospiando@gmail.com>
2019-03-04 17:40:08 +0100

    Ho provato ad aggiungere la lettura del file ma non sono riuscito perché mi da errore sul metodo strcpy per copiare le stringhe. Comunque ho scaricato un file da tsp lib su internet per fare la prova

--- PRO2/Main.c
+++ PRO2/Main.c
@@ -1,10 +1,25 @@
 #include "TSP.h"
 
+void parse_command_line(int argc, char** argv, instance *inst);
+
 int main(int argc, char **argv) {
 	printf("Ci sono %d elementi.\n", argc);
 	for (int i = 0; i < argc; i++) {
 		printf(argv[i]);
 		printf("\n");
 	}
+	instance inst;
+	parse_command_line(argc, argv, &inst);
 	return 0;
+}
+
+void parse_command_line(int argc, char** argv, instance *inst) {
+	//default configurations for instance 
+	//inst->input_file = "NULL";
+	
+	//parsing of the command line
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "-input") == 0) { strcpy(inst->input_file, argv[++i]); continue; }
+		
+	}
 }
