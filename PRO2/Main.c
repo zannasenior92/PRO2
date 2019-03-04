c8138499468a209f1d82782265feb585406a2134
marco <marcospiando@gmail.com>
2019-03-04 10:35:34 +0100

    Ho modificato il main

--- PRO2/Main.c
+++ PRO2/Main.c
@@ -1,5 +1,10 @@
-int main(int argc) {
-	//questo è il file principale
+int main(int argc, char **argv) {
+	printf("Ci sono %d elementi.\n", argc);
+	for (int i = 0; i < argc; i++) {
+		printf(argv[i]);
+		printf("\n");
+	}
+	return 0;
 }
 /*
 fviohehiuoheuiovhiowoioifvjiofjiofjiofjviovjrfiovjrfiojiofviofvjio
