8d1f2a473b89f8254eb9597bffcd72a2e92d199d
marco <marcospiando@gmail.com>
2019-03-07 16:57:16 +0100

    Aggiunto file TSP.c che conterrà funzioni di cplex

---
+++ PRO2/TSP.c
@@ -0,0 +1,11 @@
+/*
+File che contiene le funzioni che usano cplex
+*/
+#include "TSP.h";
+
+//Funzione per trovare la distanza tra due punti; ritorna un double
+double dist(int i, int j, instance *inst){
+	double dx = inst->xcoord[i] - inst->xcoord[j];
+	double dy = inst->ycoord[i] - inst->ycoord[j];
+	return sqrt(dx*dx + dy * dy);
+}
