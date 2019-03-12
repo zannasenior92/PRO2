ba50936fcf4678e09e158077337dc9831881cd38
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-03-12 17:50:59 +0100

    Aggiornato Gnuplot (ora stampa anche le linee e non serve cambiare ogni volta il numero dei comandi nel for)

--- PRO2/comandiGnuplot.c
+++ PRO2/comandiGnuplot.c
@@ -3,16 +3,29 @@
 //-------------------------COMANDS FOR GNUPLOT----------------------------------------
 void plot_coord(instance *inst) {
 	char * commandsForGnuplot[] = {
-		"set title \"Punti TSP att48\"",
+		
 		//-----------------------------PATH COLLABORATORS--------------------------------------------
 		//BASTA MODIFICARE modificare il pezzo di path da "marco" a "Luca" e viceversa
-		"plot \"C:/Users/marco/source/repos/PRO2/PRO2/coordinateAtt48.txt\" with labels point pointtype 7 offset char 1,-1.0 notitle",
+		
+		"set title \"Punti TSP att48\"",
 		"set output 'nodes.eps'",
-		"unset border",
-		"unset xtics",
-		"unset ytics",
+		"set style line 1 \	linecolor rgb '#0060ad' ", //set the color line
+		"unset border", //remove the bordes
+		"unset xtics", //remove axis x
+		"unset ytics", //remove axis y
+		"unset key", //toglie legenda path
+		"plot 'C:/Users/Luca/source/repos/PRO2/PRO2/coordinateAtt48.txt' with lp ls 1, '' with labels offset char 1,-1.0 point pointtype 7 lc rgb '#0060ad' ",
 		"exit"
 	};
+
+	//--------------------NUMBER OF GNUPLOT COMMANDS------------------------------
+	int n_commands = sizeof(commandsForGnuplot) / sizeof(commandsForGnuplot[0]);
+
+	if (VERBOSE>200)
+	{
+		printf("Numero comandi gnuplot: %d \n", n_commands);
+	}
+
 
 	FILE * temp = fopen("coordinateAtt48.txt", "w");
 	/*Opens an interface that one can use to send commands as if they were typing into the
@@ -28,9 +41,11 @@ void plot_coord(instance *inst) {
 	fclose(temp);
 
 	FILE * gnuplotPipe = _popen("C:/gnuplot/bin/gnuplot.exe -persistent", "w");
-	for (int i = 0; i < 6; i++)
+
+	for (int i = 0; i < n_commands; i++)
 	{
 		fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
+		printf("1");
 	}
 	_pclose(gnuplotPipe);
 }
