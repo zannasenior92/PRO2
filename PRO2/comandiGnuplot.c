8d7815412b9e477eef7bbb1139aaff28ec216228
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-03-22 13:10:07 +0100

    LAYOUT

--- PRO2/comandiGnuplot.c
+++ PRO2/comandiGnuplot.c
@@ -41,7 +41,7 @@ void plot_gnuplot(instance *inst) {
 		"exit"
 	};
 	/*----------------------------------------------------------------------------------*/
-	//commandsForGnuplot[1] = title;
+
 	/*--------------------NUMBER OF GNUPLOT COMMANDS------------------------------------*/
 	int n_commands = sizeof(commandsForGnuplot) / sizeof(commandsForGnuplot[0]);
 	if (VERBOSE>200)
@@ -52,25 +52,23 @@ void plot_gnuplot(instance *inst) {
 
 	
 	/*---------------------------PRINTING POINTS IN FILE--------------------------------*/
-	FILE * temp = fopen("coordinateAtt48.txt", "w");
-	/*Opens an interface that one can use to send commands as if they were typing into the
-	  gnuplot command line.  "The -persistent" keeps the plot open even after your
-	  C program terminates.
+	FILE * temp = fopen("coordinateAtt48.txt", "w"); /*Opens an interface that one can use to send commands as if they were typing into the
+	  gnuplot command line.  
 	 */
 	for (int i = 0; i < inst->nnodes; i++)
 	{
-		fprintf(temp, "%lf %lf %d \n", inst->xcoord[i], inst->ycoord[i], i+1); //Write the data to a temporary file
+		fprintf(temp, "%lf %lf %d \n", inst->xcoord[i], inst->ycoord[i], i+1);  //WRITE DATA TO A TEMPORARY FILE
 	}
 	fclose(temp);
 	/*----------------------------------------------------------------------------------*/
 	
 	
 	/*----------------USING A PIPE FOR GNUPLOT TO PRINT POINTS--------------------------*/
-	FILE * gnuplotPipe = _popen("C:/gnuplot/bin/gnuplot.exe -persistent", "w");
+	FILE * gnuplotPipe = _popen("C:/gnuplot/bin/gnuplot.exe -persistent", "w");	//"-persistent" KEEPS THE PLOT OPEN EVEN AFTER YOUR C PROGRAM QUIT
 
 	for (int i = 0; i < n_commands; i++)
 	{
-		fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
+		fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]);					//Send commands to gnuplot one by one.
 	}
 	_pclose(gnuplotPipe);
 }
