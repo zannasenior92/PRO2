899a2ca5fe322eac7996633f1ae83412c3afa0f9
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-03-28 19:09:50 +0100

    sistemate alcune cosette

--- PRO2/Parser.c
+++ PRO2/Parser.c
@@ -6,7 +6,7 @@ void parse_command_line(int argc, char** argv, instance *inst) {
 	printf("User?(marco/Luca): ");
 	char user[10];
 	strcpy(user, fgets(user, 10, stdin));
-	while ((strncmp(user, "Luca", 4) != 0) && ((strncmp(user, "marco", 5) != 0)))		//CHECK INPUT
+	while ((strncmp(user, "Luca", 4) != 0) && ((strncmp(user, "marco", 5) != 0)))					//CHECK INPUT
 	{
 		printf("INPUT ERROR! User(marco/Luca)?: ");
 		strcpy(user, fgets(user, 10, stdin));
@@ -48,7 +48,7 @@ void parse_command_line(int argc, char** argv, instance *inst) {
 		}
 		else
 		{
-			strcat(name_file, "\\source\\repos\\PRO2\\PRO2\\");							//LUCA'S PATH
+			strcat(name_file, "\\Documents\\FilesTSP\\");								//LUCA'S PATH
 		}
 		fgets(in_file, 30, stdin);														//GET FILE NAME
 		in_file[strlen(in_file) - 1] = '\0';											//UNCONSIDER \n
@@ -75,39 +75,31 @@ void read_input(instance *inst) {
 	int coord_section = 0; // =1 NODE_COORD_SECTION
 	while (fgets(line, sizeof(line), input) != NULL)
 	{
-		//printf("%s", line);
-		//fflush(NULL);
-
-		if (strlen(line) <= 1) continue; // skip the blank lines
-		par_name = strtok(line, " :"); //take space and : as delimiter e then first word is NAME
-		//printf("par_name= %s\n", par_name);
+		if (strlen(line) <= 1) continue;															// SKIP BLANK LINES
+		par_name = strtok(line, " :");																// " :" as delimiter
 
 		/*--------------------------------CHECK FILE LINES--------------------------------*/
 		if (strncmp(par_name, "DIMENSION", 9) == 0)
 		{
-			//if (inst->nnodes >= 0) print_error(" repeated DIMENSION section in input file");
-			token1 = strtok(NULL, " :"); //take the successive item of the line
-			inst->nnodes = atoi(token1); //string argument to integer
-			printf("nnodes %d\n", inst->nnodes);
+			token1 = strtok(NULL, " :");															//NULL gives the following word
+			inst->nnodes = atoi(token1);															//string argument to integer
 			inst->xcoord = (double *)calloc(inst->nnodes, sizeof(double));
 			inst->ycoord = (double *)calloc(inst->nnodes, sizeof(double));
-			inst->choosen_edge = (int *)calloc(inst->nnodes * 2, sizeof(int)); //inizializzo vettore grande 2*numero nodi
+			inst->choosen_edge = (int *)calloc(inst->nnodes * 2, sizeof(int));
 			continue;
 		}
-		/*-------------------------------------------------------------------
-		0=distanza euclidea
-		1=ATT
-		*/
+		
+		/*---------------SELECT THE RIGHT TYPE DISTANCE--------------------*/
 		if (strncmp(par_name, "EDGE_WEIGHT_TYPE", 16) == 0)
 		{
 			token1 = strtok(NULL, " :");
-			if (strncmp(token1, "EUC_2D", 6) == 0) {
+			if (strncmp(token1, "EUC_2D", 6) == 0) {												//0 = EUCLIDEAN DISTANCE
 				inst->dist_type = 0;
 			}
-			if (strncmp(token1, "ATT", 3) == 0) {
+			if (strncmp(token1, "ATT", 3) == 0) {													//1 = ATT DISTANCE
 				inst->dist_type = 1;
 			}
-			if (strncmp(token1, "GEO", 3) == 0) {
+			if (strncmp(token1, "GEO", 3) == 0) {													//2 = GEO DISTANCE
 				inst->dist_type = 2;
 			}
 
@@ -119,18 +111,17 @@ void read_input(instance *inst) {
 			coord_section = 1;
 			continue;
 		}
-		if (strncmp(par_name, "EOF", 3) == 0)
+		if (strncmp(par_name, "EOF", 3) == 0)														//END OF FILE
 		{
 			coord_section = 0;
 			break;
 		}
-		/*----------------------INSERT COORDINATES IN STRUCTURE TSP.h----------------------*/
 		if (coord_section == 1) {
-			int i = atoi(par_name) - 1; //index of the point
-			token1 = strtok(NULL, " "); //first coordinate x
-			token2 = strtok(NULL, " "); //second coordinate y
-			inst->xcoord[i] = atof(token1);//insert coordinate x
-			inst->ycoord[i] = atof(token2);//insert coordinate y
+			int i = atoi(par_name) - 1;															//FIRST COORD INDEX (-1 because indexes start from 0)
+			token1 = strtok(NULL, " ");															// x COORDINATE
+			token2 = strtok(NULL, " ");															// y COORDINATE
+			inst->xcoord[i] = atof(token1);
+			inst->ycoord[i] = atof(token2);
 			continue;
 		}
 	}
