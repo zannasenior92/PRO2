9b87f9eb49de8467786b1a912e276eb76aabc033
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-04-04 17:57:01 +0200

    Sistemati file suddivisi i modelli in file diversi

---
+++ PRO2/Default.c
@@ -0,0 +1,64 @@
+#include "TSP.h"
+
+/*********************************** DEFAULT MODEL **********************************/
+void build_model(instance *inst, CPXENVptr env, CPXLPptr lp) {
+
+	double lb = 0.0;																	//lower bound
+	double ub = 1.0;																	//upper bound
+	char binary = 'B';																	//binary variable (0 OR 1)
+																						//char continuous = 'C';
+
+	char **cname = (char **)calloc(1, sizeof(char *));									// (char **) required by cplex...
+	cname[0] = (char *)calloc(100, sizeof(char));
+
+	/*-------------------------DEFINE VARIABLES ON THE MODEL----------------------*/
+	for (int i = 0; i < inst->nnodes; i++)
+	{
+		for (int j = i + 1; j < inst->nnodes; j++)
+		{
+			double obj = dist(i, j, inst);
+			sprintf(cname[0], "x(%d,%d)", i + 1, j + 1);								//PRINT VARIABLES IN CPLEX IN .lp FILE 
+
+			/*--------------------PRINT DISTANCE d(i,j)-------------------*/
+			if (VERBOSE >= 500) {
+				printf("Distance d(%d,%d): %f \n", i + 1, j + 1, dist(i, j, inst));
+			}
+
+
+			/*----------------------INSERT VARIABLE IN CPLEX--------------*/
+			if (CPXnewcols(env, lp, 1, &obj, &lb, &ub, &binary, cname)) print_error(" wrong CPXnewcols on x var.s");
+
+			/*--------------------CHECK VARIABLE POSITION-----------------*/
+			if (CPXgetnumcols(env, lp) - 1 != xpos(i, j, inst)) print_error(" wrong position for x var.s");
+			if (VERBOSE >= 500)
+			{
+				printf("The column with i=%d e j=%d is in position %d and xpos is %d\n", i, j, CPXgetnumcols(env, lp), xpos(i, j, inst));
+			}
+		}
+	}
+
+
+	/*--------------------------------ADD CONSTRAINTS----------------------------*/
+	for (int h = 0; h < inst->nnodes; h++)
+	{
+		int lastrow = CPXgetnumrows(env, lp);										//LAST ROW NUMBER(to insert the new constraint)
+		if (VERBOSE >= 200) {
+			printf("lastrow %d\n", lastrow);
+		}
+		double maxdeg = 2.0; 	 													//DEGREE OF EVERY NODE
+		char sense = 'E'; 															// E = EQUATION
+		sprintf(cname[0], "degree(%d)", h + 1);										// NAME DEGREE CONSTRAINT INSIDE THE MODEL 
+		/*-----------------------------NEW CONSTRAINT ROW------------------------*/
+		if (CPXnewrows(env, lp, 1, &maxdeg, &sense, NULL, cname)) print_error(" wrong CPXnewrows [x1]");
+		for (int i = 0; i < inst->nnodes; i++)
+		{
+			if (i == h)																// TO SKIP x(i,i) COEFFICIENTS
+				continue;
+			else
+				/*-------SET TO 1 THE VARIABLES COEFFICIENT IN THE EQUATION------*/
+				if (CPXchgcoef(env, lp, lastrow, xpos(i, h, inst), 1.0)) print_error(" wrong CPXchgcoef [x1]");
+		}
+	}
+	/*-------------------write the cplex model in file model.lp------------------*/
+	CPXwriteprob(env, lp, "model.lp", NULL);
+}
---
+++ PRO2/Fisch.c
@@ -0,0 +1,148 @@
+#include "TSP.h"
+
+
+/*********************************** FISCH MODEL ************************************/
+void build_modelFischetti(instance *inst, CPXENVptr env, CPXLPptr lp) {
+
+	double lb = 0.0; //lower bound
+	double ub = 1.0; //upper bound
+	char binary = 'B'; //binary variable (0 OR 1)
+	//char continuous = 'C';
+
+	//Definisco cname per scrivere il modello in modo più chiaro
+	char **cname = (char **)calloc(1, sizeof(char *));		// (char **) required by cplex...
+	cname[0] = (char *)calloc(100, sizeof(char));
+
+	/*-------------------------DEFINE VARIABLES ON THE MODEL----------------------*/
+	for (int i = 0; i < inst->nnodes; i++)
+	{
+		for (int j = i + 1; j < inst->nnodes; j++)
+		{
+			double obj = dist(i, j, inst);
+			sprintf(cname[0], "x(%d,%d)", i + 1, j + 1);//print variables on cplex 
+
+			/*--------------------PRINT DISTANCE d(i,j)-------------------*/
+			if (VERBOSE >= 500) {
+				printf("Distance d(%d,%d): %f \n", i + 1, j + 1, dist(i, j, inst));
+			}
+			if (CPXnewcols(env, lp, 1, &obj, &lb, &ub, &binary, cname)) print_error(" wrong CPXnewcols on x var.s");
+			if (CPXgetnumcols(env, lp) - 1 != xpos(i, j, inst)) print_error(" wrong position for x var.s");
+
+		}
+	}
+	/*----------------INSERISCO LE Z: zvh=1 se vertice v si trova in posizione h*/
+	for (int i = 0; i < inst->nnodes; i++)
+	{
+		double lb = 0.0; //lower bound
+		double ub = 1.0; //upper bound
+		char binary = 'B'; //binary variable (0 OR 1)
+		for (int j = 0; j < inst->nnodes; j++)
+		{
+			double obj = 0;
+			sprintf(cname[0], "z(%d,%d)", i + 1, j + 1);//print variables on cplex 
+
+			if (CPXnewcols(env, lp, 1, &obj, &lb, &ub, &binary, cname)) print_error(" wrong CPXnewcols on v variables");
+
+		}
+	}
+
+
+	/*--------------------------------ADD CONSTRAINTS----------------------------*/
+	for (int h = 0; h < inst->nnodes; h++)  // degree ciclo esterno per ogni vincolo che voglio aggiungere per nodo h
+	{
+		int lastrow = CPXgetnumrows(env, lp);	//chiedo a cplex ultima riga cambiata chiedendo numero di righe
+		if (VERBOSE >= 200) {
+			printf("lastrow %d\n", lastrow);
+		}
+		double maxdeg = 2.0; 	 	//NOI vogliamo 2 uno entrante e uno uscente
+		char sense = 'E'; 			//// E equazione
+		sprintf(cname[0], "degree(%d)", h + 1);   // DO un nome NOI degree 
+		if (CPXnewrows(env, lp, 1, &maxdeg, &sense, NULL, cname)) print_error(" wrong CPXnewrows [x1]");  //Nuova riga vuota con coeff diversi da 0 e con informazioni nella posizione last row 																posizione last row
+		for (int i = 0; i < inst->nnodes; i++)		//cambio coefficienti non 0 mettendoli a 1 NOI se i=h salto istruzione, se i!=h faccio chgcoef change coeff a 1
+									// non importa se i>h perché xpos fa inversione
+		{
+			if (i == h)
+				continue;
+			else
+				/*-------------SET TO 1 THE VARIABLES COEFFICIENT IN THE EQUATION----------------------------*/
+				if (CPXchgcoef(env, lp, lastrow, xpos(i, h, inst), 1.0)) print_error(" wrong CPXchgcoef [x1]");
+		}
+	}
+	/*---------------VINCOLI PER Z-------------*/
+	/* INSERISCO z11=1*/
+	int izero = 0;
+	int *index = (int *)malloc(1 * sizeof(int));
+	double *value = (double *)malloc(1 * sizeof(double));
+	double rhs = 1.0;
+	char sense = 'E';
+	index[0] = zpos(0, 0, inst);
+	value[0] = 1.0;
+	sprintf(cname[0], "z11");
+	if (CPXaddlazyconstraints(env, lp, 1, 1, &rhs, &sense, &izero, index, value, cname)) print_error("wrong CPXlazyconstraints");
+
+	/*-------------ogni vertice deve avere una posizione---*/
+	for (int v = 0; v < inst->nnodes; v++)
+	{
+		int lastrow = CPXgetnumrows(env, lp);	//chiedo a cplex ultima riga cambiata chiedendo numero di righe
+		double rhs = 1.0;
+		char sense = 'E';
+		sprintf(cname[0], "somme_z((%d),h)", v + 1);
+		if (CPXnewrows(env, lp, 1, &rhs, &sense, NULL, cname)) print_error(" wrong CPXnewrows [x1]");
+		for (int h = 0; h < inst->nnodes; h++)
+		{
+			/*-------------SET TO 1 THE VARIABLES COEFFICIENT IN THE EQUATION----------------------------*/
+			if (CPXchgcoef(env, lp, lastrow, zpos(v, h, inst), 1.0)) print_error(" wrong CPXchgcoef [x1]");
+		}
+	}
+	/*-------------ogni posizione deve avere un vertice---*/
+	for (int h = 0; h < inst->nnodes; h++)
+	{
+		int lastrow = CPXgetnumrows(env, lp);
+		double rhs = 1.0;
+		char sense = 'E';
+		sprintf(cname[0], "somme_z(v,(%d))", h + 1);
+		if (CPXnewrows(env, lp, 1, &rhs, &sense, NULL, cname)) print_error(" wrong CPXnewrows [x1]");
+		for (int v = 0; v < inst->nnodes; v++)
+		{
+			/*-------------SET TO 1 THE VARIABLES COEFFICIENT IN THE EQUATION----------------------------*/
+			if (CPXchgcoef(env, lp, lastrow, zpos(v, h, inst), 1.0)) print_error(" wrong CPXchgcoef [x1]");
+		}
+	}
+	/*---------------Vincolo forte somme(zit)+xij+somme(jt)<=2*/
+
+	for (int i = 1; i < inst->nnodes; i++) {
+		for (int j = 1; j < inst->nnodes; j++) {
+			if (i == j) continue;
+			for (int h = 2; h < inst->nnodes; h++) {
+				int n = 0;
+				char sense = 'L';
+				int izero = 0;
+				int *index = (int *)malloc((inst->nnodes) * sizeof(int));
+				double *value = (double *)malloc((inst->nnodes) * sizeof(double));
+				double rhs = 2;
+
+				sprintf(cname[0], "h%d_x(%d,%d)", h + 1, i + 1, j + 1);
+				for (int t = 0; t < h; t++) {
+					index[n] = zpos(i, t, inst);
+					value[n] = 1.0;
+					n++;
+				}
+
+				index[n] = xpos(i, j, inst);
+				value[n] = 1.0;
+				n++;
+				for (int t = h + 1; t < inst->nnodes; t++) {
+					index[n] = zpos(j, t, inst);
+					value[n] = 1.0;
+					n++;
+				}
+				if (CPXaddlazyconstraints(env, lp, 1, n, &rhs, &sense, &izero, index, value, cname)) print_error("wrong CPXlazyconstraints");
+				free(index);
+				free(value);
+			}
+
+		}
+	}
+
+	CPXwriteprob(env, lp, "modelFischetti.lp", NULL);
+}
