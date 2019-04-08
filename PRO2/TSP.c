10edde104eed0ba4406b589a9730eefef84c41bd
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-04-08 12:01:23 +0200

    Layout sistemato. Bisogna spezzare il file TSP.c perchè troppo lungo

--- PRO2/TSP.c
+++ PRO2/TSP.c
@@ -39,23 +39,27 @@ int TSPopt(instance *inst)
 	while (!done) {
 		
 		if (CPXgettime(env, &time3)) print_error("Error getting time\n");				//GET TIME
-		//if (CPXgetdettime(env, &ticks1)) print_error("Error getting time\n");
 		if (CPXsetdblparam(env, CPX_PARAM_TILIM, timelimit)) print_error("Error on setting parameter");
+		//if (CPXgetdettime(env, &ticks1)) print_error("Error getting time\n");
 		//if (CPXsetintparam(env, CPX_PARAM_HEURFREQ, 20)) print_error("Error on setting parameter");
 		
+		/******************************************************************************************/
+		/*------------------------------------USE OF TIMELIMITS-----------------------------------*/
+		/******************************************************************************************/
+
 		resolved = 0;																	//UNTIL OUT STATE ISN'T OPTIMAL
-		
 		while (!resolved) {
 			
 			if (CPXmipopt(env, lp)) print_error("Error resolving the model\n");
-			//Guardo con che stato è terminata la risoluzione
-			int status = CPXgetstat(env, lp);
+			
+			int status = CPXgetstat(env, lp);											//GET STATUS END
 			printf("Status=%d\n", status);
-			//Se è terminata per timelimit e non ha soluzioni raddoppio il tempo (????) e continuo
+			
+			/*-------------------------------STATUS ENDED BY TIMELIMIT----------------------------*/
 			if (status == CPXMIP_TIME_LIM_INFEAS) {	
 				timelimit *= 2;
 				if (CPXsetdblparam(env, CPX_PARAM_TILIM, timelimit)) print_error("Error on setting parameter");
-				printf("No soluzione, raddoppio il tempo\n");
+				printf("No solution, double the time\n");
 				add_SEC(env, lp, inst);
 				if (inst->sec == 0) {
 					resolved = 1;
@@ -63,17 +67,20 @@ int TSPopt(instance *inst)
 				}
 			
 			}
+			/*------------------------STATUS ENDED BY IMPOSSIBLE SOLUTION-------------------------*/
 			if (status == CPXMIP_INFEASIBLE) {
-				printf("Soluzione impossibile!\n");
+				printf("Impossible solution!\n");
 				exit(0);
 			}
+
+			/*---------------STATUS ENDED BY TIMELIMIT BUT INTEGER SOLUTION FOUNDED----------------*/
 			if (status == CPXMIP_TIME_LIM_FEAS) {
 				int ncols = CPXgetnumcols(env, lp);
 				inst->best_sol = (double *)calloc(ncols, sizeof(double));				//best objective solution
 				if (CPXgetx(env, lp, inst->best_sol, 0, ncols - 1)) print_error("no solution avaialable");
 				if (CPXgetobjval(env, lp, &ottimo)) print_error("Error getting optimal value");
 				
-				printf("Miglior soluzione trovata:%.0f\n", ottimo);
+				printf("Best solution found:%.0f\n", ottimo);
 				if (kruskal_sst(env, lp, inst) == inst->nnodes) {
 					if (CPXsetdblparam(env, CPX_PARAM_TILIM, timelimit * 2)) print_error("Error on setting parameter");
 					printf("Ha n=%d comp conn\n", kruskal_sst(env, lp, inst));
@@ -91,23 +98,22 @@ int TSPopt(instance *inst)
 							done = 1;
 						}
 						if (VERBOSE >= 10) {
-							printf("Aggiunti vincoli\n");
+							printf("Added constraints\n");
 						}
 					}
 					update_choosen_edge(inst);
 					add_edge_to_file(inst);
-					plot_gnuplot(inst);
-					
-					
+					plot_gnuplot(inst);	
 				}
 
 			}
+			/*------------FOUNDED INTEGER SOLUTION OR INTEGER SOLUTION WITH TOLERANCE---------------*/
 			if ((status == CPXMIP_OPTIMAL) || (status == CPXMIP_OPTIMAL_TOL)) {
 				int ncols = CPXgetnumcols(env, lp);
-				inst->best_sol = (double *)calloc(ncols, sizeof(double));				//best objective solution
+				inst->best_sol = (double *)calloc(ncols, sizeof(double));
 				if (CPXgetx(env, lp, inst->best_sol, 0, ncols - 1)) print_error("no solution avaialable");
 				if (CPXgetobjval(env, lp, &ottimo)) print_error("Error getting optimal value");
-				printf("Miglior soluzione trovata:%.0f\n", ottimo);
+				printf("Best solution found:%.0f\n", ottimo);
 				resolved = 1;
 				if (kruskal_sst(env, lp, inst) == 1) {
 					done = 1;
@@ -118,7 +124,7 @@ int TSPopt(instance *inst)
 				else {
 					add_SEC(env, lp, inst);
 					if (VERBOSE >= 10) {
-						printf("Aggiunti vincoli\n");
+						printf("Added constraints\n");
 					}
 				}
 				update_choosen_edge(inst);
@@ -136,8 +142,8 @@ int TSPopt(instance *inst)
 		
 		
 	}
-	
-	//Ultimissima iterazione senza timelimit TODO
+
+	/*-------------------------VERY USEFUL ITERATION WITHOUT TIMELIMIT----------------------*/
 	if (CPXsetdblparam(env, CPX_PARAM_TILIM, 1e+75)) print_error("Error on setting parameter");
 	if (CPXsetintparam(env, CPX_PARAM_HEURFREQ, 0)) print_error("Error on setting parameter");
 
@@ -180,7 +186,7 @@ int TSPopt(instance *inst)
 					if (inst->best_sol[xpos_compact(i, j, inst)] > 0.5) {
 
 						if (VERBOSE >= 100) {
-							printf("Il nodo (%d,%d) e' selezionato\n", i + 1, j + 1);
+							printf("Node (%d,%d) selected\n", i + 1, j + 1);
 						}
 						/*--ADD EDGES(VECTOR LENGTH = 2*nnodes TO SAVE NODES OF EVERY EDGE)--*/
 						inst->choosen_edge[n] = i;
@@ -197,7 +203,7 @@ int TSPopt(instance *inst)
 				if (inst->best_sol[xpos(i, j, inst)] > 0.5) {
 
 					if (VERBOSE >= 100) {
-						printf("Il nodo (%d,%d) e' selezionato\n", i + 1, j + 1);
+						printf("Node (%d,%d) selected\n", i + 1, j + 1);
 					}
 					/*--ADD EDGES(VECTOR LENGTH = 2*nnodes TO SAVE NODES OF EVERY EDGE)--*/
 					inst->choosen_edge[n] = i;
@@ -249,9 +255,7 @@ int kruskal_sst(CPXENVptr env, CPXLPptr lp, instance *inst) {
 					if (inst->comp[v] == c2)
 						inst->comp[v] = c1;
 				}
-				
 			}
-
 		}
 	}
 	
@@ -260,7 +264,6 @@ int kruskal_sst(CPXENVptr env, CPXLPptr lp, instance *inst) {
 			printf("Componente %d\n", inst->comp[i]);
 		}
 		inst->mycomp[inst->comp[i]] = 1;
-
 	}
 
 	int n = 0;
@@ -268,7 +271,6 @@ int kruskal_sst(CPXENVptr env, CPXLPptr lp, instance *inst) {
 		if (inst->mycomp[i]!=0) {
 			n++;
 		}
-
 	}
 	if(VERBOSE>=10){
 		printf("Componenti connesse %d\n", n);
