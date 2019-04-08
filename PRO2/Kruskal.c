a10ed4442e4930f7ec4e09539005f725c7417e69
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-04-08 12:23:36 +0200

    Diviso il codice del TSP. L'ho messo nella sottocartella Default

---
+++ PRO2/Kruskal.c
@@ -0,0 +1,54 @@
+#include "TSP.h"
+
+
+/*---------------------------DEFINING CONNECTED COMPONENTS---------------------------*/
+int kruskal_sst(CPXENVptr env, CPXLPptr lp, instance *inst) {
+	int c1, c2 = 0;
+	int n_connected_comp = 0;
+	int max = -1;
+	inst->mycomp = (int*)calloc(inst->nnodes, sizeof(int));
+
+	/*---------------------INIZIALIZATION----------------------*/
+	for (int i = 0; i < inst->nnodes; i++) {
+		inst->comp[i] = i;
+	}
+	/*-----------------------COMPONENTS UNION------------------*/
+	for (int i = 0; i < inst->nnodes; i++) {
+		for (int j = i + 1; j < inst->nnodes; j++) {
+			if (inst->best_sol[xpos(i, j, inst)] > TOLERANCE) {
+				if (inst->comp[i] != inst->comp[j]) {
+					c1 = inst->comp[i];
+					c2 = inst->comp[j];
+				}
+				for (int v = 0; v < inst->nnodes; v++) {
+					if (inst->comp[v] == c2)
+						inst->comp[v] = c1;
+				}
+
+			}
+
+		}
+	}
+
+
+	/*--------------------COUNT COMPONENTS---------------------*/
+	for (int i = 0; i < inst->nnodes; i++) {
+		if (VERBOSE >= 100) {
+			printf("Componente %d\n", inst->comp[i]);
+		}
+		inst->mycomp[inst->comp[i]] = 1;
+	}
+	int n = 0;
+	for (int i = 0; i < inst->nnodes; i++) {
+		if (inst->mycomp[i] != 0) {
+			n++;
+		}
+	}
+	if (VERBOSE >= 100) {
+		printf("Componenti connesse %d\n", n);
+	}
+	inst->n_connected_comp = n;
+	return n;
+	/*---------------------------------------------------------*/
+
+}
