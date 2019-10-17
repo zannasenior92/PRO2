d3abe9668254c3e26d4711c70a523e109ae7bb46
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-10-17 12:22:06 +0200

    sistemato

--- PRO2/TSP.c
+++ PRO2/TSP.c
@@ -7,67 +7,11 @@
 void build_modelFlow2(instance *inst, CPXENVptr env, CPXLPptr lp);
 void add_edge_to_file(instance *inst);
 void print_error(const char *err);
-
-/*------------------POSITION OF VARIABLE INSIDE THE MODEL----------------------------*/
-int xpos(int i, int j, instance *inst) {
-	return i * inst->nnodes + j;
-}
-int xpos_compact(int i, int j, instance *inst) {
-	return i * inst->nnodes + j;
-}
-int ypos(int i, int j, instance *inst) {
-	return (inst->last_x_index+1) + i * inst->nnodes + j;
-}
-int zpos_flow2(int i, int j, instance *inst) {
-	return ((ypos(inst->nnodes - 1, inst->nnodes - 1, inst) + (i * inst->nnodes + j)) + 1);
-}
-
-
-/*-------------------------DISTANCE BETWEEN TWO POINTS-------------------------------*/
-double dist(int i, int j, instance *inst){
-	if(inst->dist_type==0) {
-		double dx = inst->xcoord[i] - inst->xcoord[j];
-		double dy = inst->ycoord[i] - inst->ycoord[j];
-		return (int)(sqrt((dx*dx + dy * dy)) + 0.5);
-	}
-	if(inst->dist_type==1){
-		double dx = inst->xcoord[i] - inst->xcoord[j];
-		double dy = inst->ycoord[i] - inst->ycoord[j];
-		double rij = sqrt((dx*dx+dy*dy)/10.0);
-		int tij = (int)(rij + 0.5);
-		if (tij < rij)
-			return (tij + 1);
-		else
-			return tij;
-	}
-	if (inst->dist_type == 2) {
-		double PI = 3.141592;
-		double deg = (int)(inst->xcoord[i]);
-		double min = inst->xcoord[i] - deg;
-		double lati = PI * (deg + 5.0*min / 3.0) / 180.0;
-		deg = (int)(inst->ycoord[i] + 0.5);
-		min = inst->ycoord[i] - deg;
-		double longi = PI * (deg + 5.0*min / 3.0) / 180.0;
-
-		deg = (int)(inst->xcoord[j]);
-		min = inst->xcoord[j] - deg;
-		double latj = PI * (deg + 5.0*min / 3.0) / 180.0;
-		deg = (int)(inst->ycoord[j] + 0.5);
-		min = inst->ycoord[j] - deg;
-		double longj = PI * (deg + 5.0*min / 3.0) / 180.0;
-
-		double RRR = 6378.388;
-		double q1 = cos(longi - longj);
-		double q2 = cos(lati - latj);
-		double q3 = cos(lati + latj);
-		int dij = (int)(RRR*acos(0.5*((1.0 + q1)*q2 - (1.0 - q1)*q3)) + 1.0);
-		return dij;
-	}
-	else print_error("Something go wrong in dist function");
-	return -1;
-}
-
-
+int xpos(int i, int j, instance *inst);
+int xpos_compact(int i, int j, instance *inst);
+int ypos(int i, int j, instance *inst);
+int zpos_flow2(int i, int j, instance *inst);
+double dist(int i, int j, instance *inst);
 
 
 /*------------------------------SOLVE THE MODEL--------------------------------------*/
@@ -94,7 +38,7 @@ int TSPopt(instance *inst)
 	/*-------------------PRINT SELECTED EDGES(remember cplex tolerance)--------------*/
 	for (int i = 0; i < inst->nnodes; i++) {
 		for (int j = 0; j < inst->nnodes; j++) {
-			if (inst->best_sol[xpos(i, j, inst)] > 0.5) {
+			if (inst->best_sol[xpos_compact(i, j, inst)] > 0.5) {
 
 				if (VERBOSE >= 1) {
 					printf("Il nodo (%d,%d) e' selezionato\n", i + 1, j + 1);
