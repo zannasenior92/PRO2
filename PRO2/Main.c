835e2877c38ac4e9bfb90b30227170bfb08bf40e
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-10-23 19:23:12 +0200

    ok

--- PRO2/Main.c
+++ PRO2/Main.c
@@ -21,8 +21,9 @@ void free_instance(instance *inst) {
 /*----------------------------------------MAIN-------------------------------------------*/
 int main(int argc, char **argv) {
 	
-	clock_t start_time, end_time;											//START TIME, END TIME
-	double elapsed_time;													//ELAPSED TIME
+	double total_time;
+	clock_t start_t, end_t;													//START TIME
+	start_t = clock();
 	instance inst;															//CREATE VARIABLE inst OF TYPE instance
 
 	
@@ -40,12 +41,20 @@ int main(int argc, char **argv) {
 		double dProva = dist(31, 39, &inst);
 		printf("Distanza tra 32 e 40 : %.2f\n", dProva);
 	}
-	
-	if (TSPopt(&inst)) print_error(" error within TSPopt()");
 
-	//plot_gnuplot(&inst);													//PLOT COORDINATES IN GNUPLOT WINDOW
+	if (TSPopt(&inst)) print_error(" error within TSPopt()");
+	plot_gnuplot(&inst);													//PLOT COORDINATES IN GNUPLOT WINDOW
 	free_instance(&inst);													//FREE MEMORY OCCUPIED BY instance TSP.h
 	
+
+	end_t = clock();														//FINAL TIME
+	total_time = ((double)(end_t - start_t)) / CLOCKS_PER_SEC;
+
+	/*-------------------------STAMP ELAPSED TIME-------------------------*/
+	if (VERBOSE >= 100)
+	{
+		printf("The program time is: %f (s)", total_time);
+	}
 
 	return 0;
 }
