32715dd8a7e780235bd133d84531fe6aad233b5f
LucaZanetti <luca.zanetti.5@studenti.unipd.it>
2019-10-24 15:18:57 +0200

    sistemato per testing

--- PRO2/Main.c
+++ PRO2/Main.c
@@ -1,4 +1,5 @@
 #include "TSP.h"
+#include <time.h>													//TO MESURE PROGRAM TIME
 #include <ilcplex/cplex.h>
 #pragma warning(disable : 4996)
 
@@ -6,13 +7,12 @@
 void parse_command_line(int argc, char** argv, instance *inst);
 void read_input(instance *inst);
 void plot_gnuplot(instance *inst);
-int TSPopt(instance *inst);
+int TSPopt(instance *inst, int i);
 double dist(int i, int j, instance *inst);
-void print_error(const char *err) { printf("\n\n ERROR: %s \n\n", err); fflush(NULL); exit(1); } 
+void print_error(const char *err) { printf("\n\n ERROR: %s \n\n", err); fflush(NULL); exit(1); }
 void free_instance(instance *inst) {
 	free(inst->xcoord);
 	free(inst->ycoord);
-	free(inst->choosen_edge);
 }
 /*---------------------------------------------------------------------------------------*/
 
@@ -20,32 +20,47 @@ void free_instance(instance *inst) {
 /*----------------------------------------MAIN-------------------------------------------*/
 int main(int argc, char **argv) {
 
-	instance inst; //we create the variable inst of type instance
 
-	parse_command_line(argc, argv, &inst); //keep the arguments of the command line
-	read_input(&inst);//use the name of the variale related to the instance to access on the field of the instance
+	instance inst;															//CREATE VARIABLE inst OF TYPE instance
+	char* instances[] = { "a280.tsp","ali535.tsp","att48.tsp","att532.tsp","berlin52.tsp","bier127.tsp","burma14.tsp","ch130.tsp",
+	"ch150.tsp","d198.tsp","d493.tsp","d657.tsp","eil51.tsp","eil76.tsp","eil101.tsp","fl417.tsp","gil262.tsp","gr202.tsp",
+	"gr229.tsp","gr431.tsp"
+	};
 
-	if(VERBOSE>=200){
+	/*,"kroA100.tsp","kroA150.tsp","kroA200.tsp","kroB100.tsp","kroB150.tsp","kroB200.tsp","kroC100.tsp",
+	"kroD100.tsp","kroE100.tsp","lin105.tsp","lin318.tsp","p654.tsp","pcb442.tsp","pr76.tsp","pr107.tsp","pr124.tsp","pr136.tsp",
+	"pr144.tsp","pr152.tsp","pr226.tsp","pr264.tsp","pr299.tsp","pr439.tsp","rat99.tsp","rat195.tsp","rat575.tsp","rat783.tsp",
+	"rd100.tsp","rd400.tsp","st70.tsp","u159.tsp","u574.tsp","u724.tsp","ulysses16.tsp","ulysses22.tsp"*/
+
+	//parse_command_line(argc, argv, &inst);									//keep the arguments of the command line
+	char name_file[100] = "";
+	for (int i = 0; i < sizeof(instances) / sizeof(instances[0]); i++) {
+
+		strcat(name_file, "C:\\Users\\Luca\\Documents\\FilesTSP\\");
+		strcat(name_file, instances[i]);
+		printf("%s\n", name_file);
+		strcpy(inst.input_file, name_file);
+		read_input(&inst);														//READ VARIABLES FROM INPUT AND SAVE INTO inst
+		if (TSPopt(&inst, i)) print_error(" error within TSPopt()");
+		strcpy(name_file, "");
+	}
+
+
+	if (VERBOSE >= 200) {
 		for (int i = 0; i < inst.nnodes; i++) {
 			printf("Capitale %d coord x:%.0f coord y:%.0f\n", i + 1, inst.xcoord[i], inst.ycoord[i]);
 		}
 
 	}
-	if(VERBOSE>=500){
+	if (VERBOSE >= 500) {
 		double dProva = dist(31, 39, &inst);
 		printf("Distanza tra 32 e 40 : %.2f\n", dProva);
 	}
 
-	if (TSPopt(&inst)) print_error(" error within TSPopt()");
-	plot_gnuplot(&inst); //plot coord in a gnuplot window
-	free_instance(&inst);//libero la memoria occupata dall'istanza creata nel file TSP
-	
+
+	//plot_gnuplot(&inst);													//PLOT COORDINATES IN GNUPLOT WINDOW
+	free_instance(&inst);													//FREE MEMORY OCCUPIED BY instance TSP.h
+
+
 	return 0;
 }
-
-
-
-
-
-
-/*--------------------------------------------------------*/
--- PRO2/TSP.c
+++ PRO2/TSP.c
@@ -4,35 +4,55 @@
 
 
 /*-----------------------------FUNCTIONS & METHODS-----------------------------------*/
-void build_model(instance *inst, CPXENVptr env, CPXLPptr lp);
+void build_modelPers(instance *inst, CPXENVptr env, CPXLPptr lp);
 void print_error(const char *err);
 void add_edge_to_file(instance *inst);
 int xpos(int i, int j, instance *inst);
 
 
-
-
-
-
 /*------------------------------SOLVE THE MODEL--------------------------------------*/
-int TSPopt(instance *inst)
+int TSPopt(instance *inst, int i)
 {
-	
-	int error;
-	CPXENVptr env = CPXopenCPLEX(&error); //create the environment(env)
-	CPXLPptr lp = CPXcreateprob(env, &error, "TSP"); //create the structure for our model(lp)
-	build_model(inst, env, lp); //populate the model
-	CPXsetintparam(env, CPX_PARAM_SCRIND, CPX_ON);							//to visualize in video
-	if (CPXmipopt(env, lp)) print_error("Error resolving the model\n"); //CPXmipopt to solve the model
 
+	int error;
+	CPXENVptr env = CPXopenCPLEX(&error);									//create the environment(env)
+	CPXLPptr lp = CPXcreateprob(env, &error, "TSP");						//create the structure for our model(lp)
+	CPXsetintparam(env, CPX_PARAM_RANDOMSEED, 123456);
+	CPXsetdblparam(env, CPX_PARAM_TILIM, 3600);
+	CPXsetintparam(env, CPX_PARAM_CLOCKTYPE, 2);
+	double start_time, end_time, elapsed_time;
+	if (CPXgettime(env, &start_time)) print_error("error getting time\n");
+
+	build_modelPers(inst, env, lp);												//populate the model
+	//CPXsetintparam(env, CPX_PARAM_SCRIND, CPX_ON);							//to visualize in video
+	if (CPXmipopt(env, lp)) print_error("Error resolving the model\n");		//CPXmipopt to solve the model
+
+	if (CPXgettime(env, &end_time)) print_error("error getting time\n");
+	elapsed_time = end_time - start_time;
+
+	int status = CPXgetstat(env, lp);
 	int ncols = CPXgetnumcols(env, lp);
-	if (CPXgetobjval(env, lp, &inst->best_obj_val)) print_error("no best solution avaialable");
-	printf("Miglior soluzione=%.0f\n", inst->best_obj_val);
-	inst->best_sol= (double *)calloc(ncols, sizeof(double)); //best objective solution
-	if (CPXgetx(env, lp, inst->best_sol, 0, ncols - 1)) print_error("no solution avaialable");
-	
-	if(VERBOSE>=200){
-		for (int i = 0; i < ncols - 1; i++){
+	//printf("numero colonne %d\n", ncols);
+	inst->best_sol = (double *)calloc(ncols, sizeof(double));				//best objective solution
+	if (CPXgetx(env, lp, inst->best_sol, 0, ncols - 1)) {
+		//print_error("no solution avaialable");
+		inst->input_file_name[strlen(inst->input_file_name) - 1] = '\0';
+		char out_file[100] = "";
+		strcat(out_file, "file");
+		char iter[2];
+		sprintf(iter, "%d", i);
+		strcat(out_file, iter);
+		strcat(out_file, ".txt");
+		FILE* output = fopen(out_file, "w");
+		fprintf(output, "MTZ,%s,%f,0,123456", inst->input_file_name, elapsed_time);
+		fclose(output);
+		CPXfreeprob(env, &lp);
+		CPXcloseCPLEX(&env);
+		return 0;
+	}
+
+	if (VERBOSE >= 200) {
+		for (int i = 0; i < ncols - 1; i++) {
 			printf("Best %f\n", inst->best_sol[i]);
 		}
 	}
@@ -40,31 +60,49 @@ int TSPopt(instance *inst)
 	int n = 0;
 	/*-------------------PRINT SELECTED EDGES(remember cplex tolerance)--------------*/
 	for (int i = 0; i < inst->nnodes; i++) {
-		for (int j = i + 1; j < inst->nnodes; j++) {
-			if (inst->best_sol[xpos(i, j, inst)] > 0.5){
-				
-				if(VERBOSE>=100){
-					printf("Il nodo (%d,%d) e' selezionato\n", i+1, j+1);
+		for (int j = 0; j < inst->nnodes; j++) {
+			if (inst->best_sol[xpos_compact(i, j, inst)] > 0.5) {
+
+				if (VERBOSE >= 100) {
+					printf("Il nodo (%d,%d) e' selezionato\n", i + 1, j + 1);
 				}
 				/*--ADD EDGES(VECTOR LENGTH = 2*nnodes TO SAVE NODES OF EVERY EDGE)--*/
-				inst->choosen_edge[n] = i;	
-				inst->choosen_edge[n+1] = j;
+				inst->choosen_edge[n] = i;
+				inst->choosen_edge[n + 1] = j;
 				n += 2;
 				count++;
 			}
 		}
 	}
-	add_edge_to_file(inst);
+	//add_edge_to_file(inst);
+
 	if (VERBOSE >= 100) {
 		printf("Selected nodes: %d \n", count);
 	}
-	
-	
 	/*-------------------------------------------------------------------------------*/
+	/*-----------------------FIND AND PRINT THE OPTIMAL SOLUTION---------------------*/
+	if (CPXgetobjval(env, lp, &inst->best_obj_val)) print_error("no best objective function");	//OPTIMAL SOLUTION FOUND
+	printf("Object function optimal value is: %f\n", inst->best_obj_val);
 
-	/*------------------------------CLEAN AND CLOSE THE CPLEX ENVIROENMENT-----------*/
+	/*------------------------------CLEAN AND CLOSE THE CPLEX ENVIRONMENT-----------*/
 	CPXfreeprob(env, &lp);
 	CPXcloseCPLEX(&env);
+	inst->input_file_name[strlen(inst->input_file_name) - 1] = '\0';
+	char out_file[100] = "";
+	strcat(out_file, "file");
+	char iter[2];
+	sprintf(iter, "%d", i);
+	strcat(out_file, iter);
+	strcat(out_file, ".txt");
+	FILE* output = fopen(out_file, "w");
+	if ((status == 101) || (status == 102)) {
+		fprintf(output, "MTZ,%s,%f,1,123456", inst->input_file_name, elapsed_time);
+	}
+	else {
+		fprintf(output, "MTZ,%s,%f,0,123456", inst->input_file_name, elapsed_time);
+
+	}
+	fclose(output);
 	return 0;
 }
 
